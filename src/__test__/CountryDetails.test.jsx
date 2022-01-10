476deb32ec4146e4bff7f893fb9d659a6a51a021
unknown <ahrafsani@gmail.com>
2022-01-11 00:24:38 +0600

    first commit

---
+++ src/__test__/CountryDetails.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import {createMemoryHistory} from "history"
+import { render, screen } from '@testing-library/react';
+import CountryDetails from '../component/CountryDetails';
+
+test('renders country Info', () => {
+    const history = createMemoryHistory();
+      history.push("/CountryDetails/name");
+
+   render(<Router history={history}>
+           <CountryDetails/>
+         </Router>);
+
+  const details = screen.getByTestId("details");
+  expect(details).toBeInTheDocument();
+});
